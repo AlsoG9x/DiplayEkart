66913bbeba9023ec079100625290ea4e68f82fd6
AlsoG9x <AlsoG9x@gmail.com>
2016-05-11 08:11:38 +0000

    modif du code pour intégrer les différents input

--- Interface_Conducteur.py
+++ Interface_Conducteur.py
@@ -2,34 +2,41 @@
 # -*- coding: utf8 -*-
 
 
-import random, os, time
+import time
 
-turns = 6 #nombre de tours donner par le conducteur
+fileTurns = open('turns.txt', 'r')
+turns = fileTurns.read()
 max_turns = turns
-battery = 47
 
 while 1:
-    file = open('variables.txt','w')
-    
-    #Quand le Capteur à effet Hall détecte la vitesse on l'affiche ici
-    speed = random.randrange(0,30)
-    print "\t","speed: ", speed, "km/h"
-    file.write(str(speed) + '\n')
-    
-    #Quand le Capteur de tours détecte un tours on décrémente la variable
-    print "\t","turns: ", turns,"/", max_turns
-    file.write(str(turns) + '\n')
+	file = open('variables.txt','w')
+	
+	fileSpeed = open('vitesse1.txt', 'r')
+	speed = fileSpeed.read()
+	fileSpeed.close()	
 
-    #Quand la tension batteries diminue on décrémente la variable
-    print "\t","battery: ", battery, "%"
-    file.write(str(battery) + '\n')
+	fileRfid = open('rfid.txt', 'r')
+	rfid = fileRfid.read()
+	fileRfid.close()
+	
+	fileBattery = open('battery.txt', 'r')
+	battery = fileBattery.read() 
+	file.Battery.close()
 
-    file.close()
+	turns = fileTurns.read()
+	fileTurns.close()
+	
 
-    print "\n" * 10 #On 'clear' l'écran à chaque fois
-    if(speed > 15):
-        battery = battery - 1 #Simulation
-    if(speed > 26):
-        turns = turns - 1 #Simulation
-    time.sleep(1)
-    #Attendre 1 seconde sinon l'écran est clear trop souvent
+	print "\t","speed: ", speed, "km/h"
+	file.write(str(speed) + '\n')
+
+	print "\t","turns: ", turns,"/", max_turns
+	file.write(str(turns) + '\n')
+
+	print "\t","battery: ", battery, "%"
+	file.write(str(battery) + '\n')
+
+	file.close()
+
+	print "\n" * 10 #Clear de l'écran
+	time.sleep(1) #Wait  1 sec
